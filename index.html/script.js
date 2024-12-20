15205ed72fb8de6b13cde7d09183578bcd6e00a3
Hamassaaa <mohamrdhesham2121@gmail.com>
2024-12-20 16:29:51 +0200

    Add files via upload

---
+++ index.html/script.js
@@ -0,0 +1,24 @@
+
+var text = prompt("write your article post ?");
+
+document.write( "<h1>your article is : <br> </h1>" + text.slice(0,20).toLocaleUpperCase());
+
+
+
+
+// Get the theme toggle icon
+var icon = document.getElementById("icon");
+
+// Add click event listener
+icon.onclick = function () {
+    // Toggle dark theme
+    document.body.classList.toggle("dark-theme");
+
+    // Change icon based on the theme
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "/image/moon.png"; // Icon for light mode
+    } else {
+        icon.src = "image/sun.webp"; // Icon for dark mode
+    }
+};
+
