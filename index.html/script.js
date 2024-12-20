905eb2119764178e8ec7adde8f29b9ebb8075c9b
Hamassaaa <mohamrdhesham2121@gmail.com>
2024-12-20 16:34:19 +0200

    Update script.js

--- index.html/script.js
+++ index.html/script.js
@@ -16,7 +16,7 @@ icon.onclick = function () {
 
     // Change icon based on the theme
     if (document.body.classList.contains("dark-theme")) {
-        icon.src = "/image/moon.png"; // Icon for light mode
+        icon.src = "https://github.com/Hamassaaa/dark-white/blob/main/index.html/image/moon.png"; // Icon for light mode
     } else {
         icon.src = "https://github.com/Hamassaaa/dark-white/blob/main/index.html/image/sun.webp"; // Icon for dark mode
     }
